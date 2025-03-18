758b58ef72f01e540edb55b1d9a5fba452b94a54
Ricardo Augusto Coelho <114035092+NlCoelho@users.noreply.github.com>
2025-03-18 01:17:05 +0000

    primeiro commit do meu projeto

---
+++ pages/modelo.js
@@ -0,0 +1,11 @@
+export default function sukuna() {
+  return (
+    <>
+      <h1>por que eu gosto do sukuna</h1>
+      <p>
+        eu gosto do sukuna porque ele é forte e poderoso mas perde para o
+        saitama.
+      </p>
+    </>
+  );
+}
---
+++ pages/senka.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <>
+      <h1>SENKA</h1>
+      <p>
+        Senku Ishigami é o protagonista da série de mangás Dr. Stone, de
+        Riichiro Inagaki e Boichi . Começando em abril de 5738 d.C., já se
+        passaram mais de 3.700 anos desde que um misterioso flash petrificou
+        quase toda a vida humana. Um gênio de 16 anos chamado Senku Ishigami é
+        repentinamente revivido para se encontrar em um mundo onde todos os
+        vestígios da civilização humana foram erodidos pelo tempo. Senku monta
+        um acampamento base e começa a estudar os humanos petrificados para
+        determinar a causa do evento.
+      </p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTey8yVYxs_1E7g8J4fmJrEFzQ0fALvGNFs7g&s" />
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAUkpCWGXPy_10DGdkFgWyj9zv8VDW8NDcYQ&s" />
+    </>
+  );
+}
